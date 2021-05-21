f5d52ffc82ce0f5a371b85dbdbf29d41c9718243
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2021-05-21 20:01:49 -0300

    initChart

--- lib/coin_portfolio_web/components/balances.ex
+++ lib/coin_portfolio_web/components/balances.ex
@@ -8,77 +8,80 @@ defmodule BalancesComponent do
     <div class="ui p-0 segment mb-1">
       <div id="balance"></div>
       <script>
+      function initChart() {
+        var dates = JSON.parse('<%= raw balance_history_to_chart_data(@balance_history) %>')
+        var formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: "<%= @current_user.main_currency %>",
+          currencyDisplay: 'narrowSymbol',
+          minimumFractionDigits: 0
+        });
 
-      setTimeout(() => {
-          var dates = JSON.parse('<%= raw balance_history_to_chart_data(@balance_history) %>')
-          var formatter = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: "<%= @current_user.main_currency %>",
-            currencyDisplay: 'narrowSymbol',
-            minimumFractionDigits: 0
-          });
-
-          var options = {
-            series: [{
-              name: 'Balance',
-              data: dates
-            }],
-            chart: {
-              type: 'area',
-              stacked: false,
-              height: 350,
-              zoom: {
-                type: 'x',
-                enabled: true,
-                autoScaleYaxis: true
-              },
-              toolbar: {
-                autoSelected: 'zoom'
-              }
+        var options = {
+          series: [{
+            name: 'Balance',
+            data: dates
+          }],
+          chart: {
+            type: 'area',
+            stacked: false,
+            height: 350,
+            zoom: {
+              type: 'x',
+              enabled: true,
+              autoScaleYaxis: true
             },
-            dataLabels: {
-              enabled: false
+            toolbar: {
+              autoSelected: 'zoom'
+            }
+          },
+          dataLabels: {
+            enabled: false
+          },
+          markers: {
+            size: 0,
+          },
+          fill: {
+            type: 'gradient',
+            gradient: {
+              shadeIntensity: 1,
+              inverseColors: false,
+              opacityFrom: 0.5,
+              opacityTo: 0,
+              stops: [0, 90, 100]
             },
-            markers: {
-              size: 0,
-            },
-            fill: {
-              type: 'gradient',
-              gradient: {
-                shadeIntensity: 1,
-                inverseColors: false,
-                opacityFrom: 0.5,
-                opacityTo: 0,
-                stops: [0, 90, 100]
+          },
+          yaxis: {
+            labels: {
+              formatter: function (val) {
+                return formatter.format(val);
               },
             },
-            yaxis: {
-              labels: {
-                formatter: function (val) {
-                  return formatter.format(val);
-                },
-              },
-              title: {
-                text: 'Holdings (<%= @current_user.main_currency %>)'
-              }
+            title: {
+              text: 'Holdings (<%= @current_user.main_currency %>)'
+            }
+          },
+          xaxis: {
+            type: 'datetime',
+            title: {
+              text: 'Time'
             },
-            xaxis: {
-              type: 'datetime',
-              title: {
-                text: 'Time'
-              },
-            },
-            tooltip: {
-              shared: false,
-              y: {
-                formatter: function (val) {
-                  return "<%= @current_user.main_currency %> " + formatter.format(val)
-                }
+          },
+          tooltip: {
+            shared: false,
+            y: {
+              formatter: function (val) {
+                return "<%= @current_user.main_currency %> " + formatter.format(val)
               }
             }
-          };
-          var chart = new ApexCharts(document.querySelector("#balance"), options);
-          chart.render();
+          }
+        };
+        var chart = new ApexCharts(document.querySelector("#balance"), options);
+        chart.render();
+      }
+      initChart()
+      setTimeout(() => {
+        initChart()
         }, 500);
       </script>
     </div>
