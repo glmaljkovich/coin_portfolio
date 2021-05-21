355eda0e8d966bd7e3be46d7da7c12132c7da6e8
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2021-05-21 19:59:18 -0300

    remove unnecessary assets

--- test/coin_portfolio/accounts_test.exs
+++ test/coin_portfolio/accounts_test.exs
@@ -31,7 +31,6 @@ defmodule CoinPortfolio.AccountsTest do
 
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
-      assert user.cmc_api_key == "some cmc_api_key"
       assert user.email == "some email"
       assert user.first_name == "some first_name"
       assert user.last_name == "some last_name"
@@ -46,7 +45,6 @@ defmodule CoinPortfolio.AccountsTest do
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
-      assert user.cmc_api_key == "some updated cmc_api_key"
       assert user.email == "some updated email"
       assert user.first_name == "some updated first_name"
       assert user.last_name == "some updated last_name"
